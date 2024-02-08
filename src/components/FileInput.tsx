0fb60b8b30bf3bbf156988b432c4fae57601ad8d
Dimas Capelari <97951537+dimascapelari@users.noreply.github.com>
2024-02-08 20:36:24 -0300

    dropzone add and remove

--- src/components/FileInput.tsx
+++ src/components/FileInput.tsx
@@ -1,15 +1,29 @@
 import { useCallback, useState } from "react";
-import { useDropzone } from "react-dropzone";
-import { Flex, Text } from "@chakra-ui/react";
+import { DropzoneState, useDropzone } from "react-dropzone";
+import { Button, Flex, Text } from "@chakra-ui/react";
+import AddRoundedIcon from "@mui/icons-material/AddRounded";
+import InsertDriveFileOutlinedIcon from "@mui/icons-material/InsertDriveFileOutlined";
+import CloseRoundedIcon from "@mui/icons-material/CloseRounded";
+
+interface InputProps {
+  dropzone: DropzoneState;
+}
+
+interface HasFileProps {
+  file?: File;
+  removeFile: () => void;
+}
 
 export const FileInput = () => {
   const [file, setFile] = useState<File | null>(null);
 
+  const removeFile = useCallback(() => {
+    setFile(null);
+  }, [file]);
+
   const onDrop = useCallback((files: File[]) => {
     setFile(files[0]);
   }, []);
-
-  if (file) return null;
 
   const dropzone = useDropzone({
     onDrop,
@@ -18,6 +32,15 @@ export const FileInput = () => {
     },
   });
 
+  // if (file) return null;
+  if (file) return <HasFile file={file} removeFile={removeFile} />;
+
+  return <InputDrop dropzone={dropzone} />;
+};
+
+const InputDrop = ({ dropzone }: InputProps) => {
+  const { getRootProps, getInputProps, isDragActive } = dropzone;
+
   return (
     <Flex
       bg="#fff"
@@ -25,7 +48,7 @@ export const FileInput = () => {
       h="326px"
       borderRadius="16px"
       flexDirection="column"
-      {...dropzone.getRootProps()}
+      {...getRootProps()}
     >
       <Flex justifyContent="space-between" w="100%" p="40px">
         <Text fontSize="24px" fontWeight="700">
@@ -37,7 +60,7 @@ export const FileInput = () => {
       </Flex>
       <Flex
         borderRadius="8px"
-        border="2px dashed #B0B0B0"
+        border={isDragActive ? "2px dashed #E7E7E7" : "2px dashed #B0B0B0"}
         w="716px"
         h="110px"
         margin="0 auto"
@@ -57,16 +80,58 @@ export const FileInput = () => {
             justifyContent="center"
             h="100%"
           >
-            <Text fontSize="16px" color="#616161">
-              Arraste ou Selecione um Arquivo
-            </Text>
-            <Text fontSize="14px" color="#B0B0B0">
-              Faça upload de arquivos .xls ou .csv
-            </Text>
+            <AddRoundedIcon
+              // sx={{ color: isDragActive ? "#B0B0B0" : "#616161" }}
+              sx={{ color: isDragActive ? "#B0B0B0" : "#616161" }}
+            />
+
+            {isDragActive ? (
+              <Text fontSize="16px" color="#616161">
+                Solte para adicionar
+              </Text>
+            ) : (
+              <>
+                <Text fontSize="16px" color="#616161">
+                  Arraste ou Selecione um Arquivo
+                </Text>
+                <Text fontSize="14px" color="#B0B0B0">
+                  Faça upload de arquivos .xls ou .csv
+                </Text>
+              </>
+            )}
           </Flex>
         </label>
 
-        <input hidden {...dropzone.getInputProps()} />
+        <input hidden {...getInputProps()} />
+      </Flex>
+    </Flex>
+  );
+};
+
+const HasFile = ({ file, removeFile }: HasFileProps) => {
+  return (
+    <Flex
+      borderRadius="8px"
+      border="2px dashed #B0B0B0"
+      w="716px"
+      h="110px"
+      margin="0 auto"
+      bg="#fff"
+      _hover={{ border: "2px dashed #E7E7E7" }}
+    >
+      <Flex
+        w="100%"
+        h="100%"
+        borderRadius="8px"
+        justifyContent="center"
+        alignItems="center"
+        gap="10px"
+      >
+        <InsertDriveFileOutlinedIcon />
+        <span>{file?.name}</span>
+        <Button type="button" onClick={removeFile}>
+          <CloseRoundedIcon />
+        </Button>
       </Flex>
     </Flex>
   );
